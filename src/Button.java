981ac42e840875d4b0f0c3c976ef92bd1132b704
Sergey <sergey198828@mail.ru>
2015-12-25 22:24:23 +0300

    First commit

---
+++ src/Button.java
@@ -0,0 +1,15 @@
+import Commands.AbstractCommand;
+
+public class Button {
+	
+AbstractCommand command;
+
+public Button(AbstractCommand command){
+	this.command=command;
+}
+
+public void press(){
+	command.execute();
+}
+
+}
---
+++ src/Commands/OnCommand.java
@@ -0,0 +1,17 @@
+package Commands;
+
+import Devices.ElectronicDevice;
+
+public class OnCommand extends AbstractCommand {
+
+	public OnCommand(ElectronicDevice device) {
+		super(device);
+	}
+
+	@Override
+	public void execute() {
+		this.device.turnOn();
+
+	}
+
+}
